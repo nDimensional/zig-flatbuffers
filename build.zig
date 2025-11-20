584a4a1211443fac7ecbc21103b5d0b6375b8096
Joel Gustafson <joelgustafson@protonmail.com>
2025-11-19 16:21:38 -0800

    remove src/codegen.zig

--- build.zig
+++ build.zig
@@ -4,26 +4,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    {
-        const codegen_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .root_source_file = b.path("src/codegen.zig"),
-        });
-
-        const codegen = b.addExecutable(.{
-            .name = "codegen",
-            .root_module = codegen_module,
-        });
-
-        const codegen_run = b.addRunArtifact(codegen);
-        if (b.args) |args|
-            codegen_run.addArgs(args);
-
-        b.installArtifact(codegen);
-        b.step("generate", "generate").dependOn(&codegen_run.step);
-    }
-
     const parse_module = b.createModule(.{
         .target = target,
         .optimize = optimize,
