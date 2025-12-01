c46e54eb6ec48a9e729928a10727eaf2cb4b93b8
Joel Gustafson <joelgustafson@protonmail.com>
2025-12-01 14:35:04 -0500

    add parse step

--- build.zig
+++ build.zig
@@ -10,18 +10,37 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const codegen_module = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-        .root_source_file = b.path("src/codegen.zig"),
-        .imports = &.{
-            .{ .name = "flatbuffers", .module = flatbuffers },
-        },
+    const parse = b.addExecutable(.{
+        .name = "zfbs-parse",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = b.path("src/parse.zig"),
+            .imports = &.{
+                .{ .name = "flatbuffers", .module = flatbuffers },
+            },
+        }),
     });
+
+    b.installArtifact(parse);
+
+    const parse_run = b.addRunArtifact(parse);
+    if (b.args) |args|
+        parse_run.addArgs(args);
+
+    b.installArtifact(parse);
+    b.step("parse", "Parse a .bfbs schema into ZON IR").dependOn(&parse_run.step);
 
     const codegen = b.addExecutable(.{
         .name = "generate",
-        .root_module = codegen_module,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = b.path("src/codegen.zig"),
+            .imports = &.{
+                .{ .name = "flatbuffers", .module = flatbuffers },
+            },
+        }),
     });
 
     b.installArtifact(codegen);
@@ -33,21 +52,24 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(codegen);
     b.step("generate", "generate").dependOn(&codegen_run.step);
 
-    // // example.zig
+    // example.zig
 
-    // const example = b.addExecutable(.{
-    //     .name = "example",
-    //     .root_module = b.createModule(.{
-    //         .target = target,
-    //         .optimize = optimize,
-    //         .root_source_file = b.path("example.zig"),
-    //     }),
-    // });
+    const example = b.addExecutable(.{
+        .name = "example",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = b.path("example.zig"),
+            .imports = &.{
+                .{ .name = "flatbuffers", .module = flatbuffers },
+            },
+        }),
+    });
 
-    // b.installArtifact(example);
+    b.installArtifact(example);
 
-    // const example_run = b.addRunArtifact(example);
-    // b.step("run", "run example.zig").dependOn(&example_run.step);
+    const example_run = b.addRunArtifact(example);
+    b.step("run", "run example.zig").dependOn(&example_run.step);
 
-    // b.step("check", "Check if example.zig compiles").dependOn(&example.step);
+    b.step("check", "Check if example.zig compiles").dependOn(&example.step);
 }
